5b7ca5c90f360f6ce00da370974b593cd13f8f64
ZavalihinAS <98523884+ZavalihinAS@users.noreply.github.com>
2022-01-27 15:46:38 +0500

    Delete menu.inc.php

--- menu.inc.php
+++
@@ -1,8 +0,0 @@
-            <nav>
-                <ul>
-                    <li><a href="index.php"> Главная </a></li>  
-                    <li><a href="req.php"> Требования к заданию </a></li>
-                    <li><a href="know.php"> Знания </a></li>
-                    <li><a href="about.php"> Обо мне </a></li>
-                </ul>    
-            </nav>
